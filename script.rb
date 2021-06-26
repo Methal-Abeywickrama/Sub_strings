ab772da6f1da6dfcd7a9aa8455ff3f5c3a3f5939
Methal Abeywickrama <methal8779@gmail.com>
2021-06-26 12:07:45 +0530

    working

--- script.rb
+++ script.rb
@@ -2,12 +2,12 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 
 def substring(words, list)
     word_array = words.split
-    reduced_list = list.reduce(Hash.new(0)) |equal_list, item| do 
-        matching_words = words.select {|word| word.include?(item)}
+    reduced_list = list.reduce(Hash.new(0)) do |equal_list, item| 
+        matching_words = word_array.select {|word| word.include?(item)}
         equal_list[item] = matching_words.length
         equal_list
         end
-    reduced_list
+    reduced_list = reduced_list.select {|k, v| v != 0}
 end
 
 p substring("Howdy partner, sit down! How's it going?", dictionary)
